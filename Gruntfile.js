9e5cc8a10aed42ee3b8ec87c168db7a554557b26
André Gaul <gaul@web-yard.de>
2013-10-13 18:43:46 +0200

    include grunt-couch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,43 @@
 module.exports = function(grunt) {
+  var config = grunt.file.readJSON('config.json');
+
+  var couch = grunt.option('couch') || 'localhost';
+  var couchpushopts = null;
+  if (couch) {
+    couchpushopts = {
+      options: {
+        user: config.couches[couch].user,
+        pass: config.couches[couch].pass
+      }
+    };
+    couchpushopts[couch] = {};
+    var files = {};
+    files[config.couches[couch].database] = 'tmp/libremap-api.json';
+    couchpushopts[couch] = { files: files};
+  };
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     // lint js files
     jshint: {
-      files: ['ddoc/**/*.js', '!ddoc/views/lib/underscore-min.js']
-    }
+      files: ['ddoc/**/*.js']
+    },
+    'couch-compile': {
+      'libremap-api': {
+        options: {
+          merge: 'ddoc-vendor'
+        },
+        files: {
+          'tmp/libremap-api.json': 'ddoc'
+        }
+      }
+    },
+    'couch-push': couchpushopts
   });
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-couch');
 
   // Default task(s).
   grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('push', ['couch']);
 };
