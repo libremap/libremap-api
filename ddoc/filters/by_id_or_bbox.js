5c262da52c08ef896dde3b28779a7a034e91725c
André Gaul <gaul@web-yard.de>
2013-11-22 20:25:16 +0100

    adapt to couchmap filter

--- ddoc/filters/by_id_or_bbox.js
+++ ddoc/filters/by_id_or_bbox.js
@@ -1,20 +1,8 @@
 function(doc, req){
-  var common = require('views/lib/libremap-common');
+  var common = require('views/lib/couchmap-common');
 
-  var parameters = JSON.parse(req.body);
-
-  // sanitize input
-
-  if (!common.isType(parameters.ids, 'array')) {
-    parameters.ids = [];
+  if (doc.type=='router') {
+    return common.filter_id_or_bbox(doc, req);
   }
-  var bbox = common.bbox(parameters.bbox);
-
-  if (parameters.ids.indexOf(doc._id)>=0 ||
-      doc.type=="router" && bbox &&
-      bbox.contains(doc.location.lat, doc.location.lon)) {
-    return true;
-  }
-
   return false;
 }
